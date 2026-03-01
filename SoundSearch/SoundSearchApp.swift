da8cd04e206e37ae4b03397dcc08fa0bffa03bce
Simon Tan <stan322@e.ntu.edu.sg>
2026-03-02 05:27:34 +0800

    Initial Commit

---
+++ SoundSearch/SoundSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundSearchApp.swift
+//  SoundSearch
+//
+//  Created by Simon Tan on 2/3/26.
+//
+
+import SwiftUI
+
+@main
+struct SoundSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
